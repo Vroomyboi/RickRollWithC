8d28bfa567526bbef26858734d5af57f8b66508f
Vroomyboi <107228708+Vroomyboi@users.noreply.github.com>
2022-06-13 20:52:26 -0400

    Add files via upload

---
+++ RickRoll.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    system("open https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    system("xdg-open https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    system("start https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    return 0;
+}
