66775ae8e86d0c3cbad3c625ed1b18af80440e70
Vroomyboi <107228708+Vroomyboi@users.noreply.github.com>
2022-06-13 20:56:36 -0400

    Add Comments Explaining Links

--- RickRoll.c
+++ RickRoll.c
@@ -3,8 +3,8 @@
 
 int main()
 {
-    system("open https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-    system("xdg-open https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-    system("start https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    system("open https://www.youtube.com/watch?v=dQw4w9WgXcQ"); // Launch "Never Gonna Give You Up" YouTube music video on Windows 
+    system("xdg-open https://www.youtube.com/watch?v=dQw4w9WgXcQ"); // Launch "Never Gonna Give You Up" YouTube video on Linux
+    system("start https://www.youtube.com/watch?v=dQw4w9WgXcQ"); // Launch "Never Gonna Give You Up" YouTube video on macOS
     return 0;
 }
